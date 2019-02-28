6b89e112c4933c8a7a6055d4f02ce8ad4e63e350
mik <mklimoff222@gmail.com>
2019-02-28 21:11:27 +0200

    something funky goes on with Go language

--- go_problem_test.go
+++ go_problem_test.go
@@ -9,22 +9,26 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"strings"
 	"testing"
 )
+
+func somethingFunkyGoesOn(data [][]string) []string {
+	resultStrings := []string{}
+	for _, item := range data {
+		row := []byte{}
+		for _, subItem := range item {
+			row = append(row, []byte(subItem)...)
+			row = append(row, []byte(" ")...)
+		}
+		resultStrings = append(resultStrings, string(row[:len(row)-1]))
+	}
+	return resultStrings
+}
 
 // this was smth ephemeral
 func TestByteProblems(t *testing.T) {
 	input := []string{"a a a a a a a", "aa a a a a a", "a aa a a a a", "a a aa a a a", "aa aa a a a", "aaaa a a a", "a a a aa a a", "aa a aa a a", "a aa aa a a", "a aaaa a a", "a a a a aa a", "aa a a aa a", "a aa a aa a", "a a aa aa a", "aa aa aa a", "aaaa aa a", "a a aaaa a", "aa aaaa a", "a a a a a aa", "aa a a a aa", "a aa a a aa", "a a aa a aa", "aa aa a aa", "aaaa a aa", "a a a aa aa", "aa a aa aa", "a aa aa aa", "a aaaa aa", "a a a aaaa", "aa a aaaa", "a aa aaaa"}
 
-	output := []string{}
-	for _, row := range input {
-		row2 := []byte{}
-		for _, cell := range strings.Split(row, " ") {
-			row2 = append(row2, []byte(cell)...)
-			row2 = append(row2, ' ')
-		}
-		output = append(output, string(row2[:len(row2)-1]))
-	}
-	assert.Equal(t, input, output)
+	another_input := [][]string{[]string{"a", "a", "a", "a", "a", "aa", "a"}, []string{"aa", "a", "a", "a", "a", "a"}, []string{"a", "aa", "a", "a", "a", "a"}, []string{"a", "a", "aa", "a", "a", "a"}, []string{"aa", "aa", "a", "a", "a"}, []string{"aaaa", "a", "a", "a"}, []string{"a", "a", "a", "aa", "a", "a"}, []string{"aa", "a", "aa", "a", "a"}, []string{"a", "aa", "aa", "a", "a"}, []string{"a", "aaaa", "a", "a"}, []string{"a", "a", "a", "aa", "aa", "a"}, []string{"aa", "a", "a", "aa", "a"}, []string{"a", "aa", "a", "aa", "a"}, []string{"a", "a", "aa", "aa", "a"}, []string{"aa", "aa", "aa", "a"}, []string{"aaaa", "aa", "a"}, []string{"a", "a", "aaaa", "a"}, []string{"aa", "aaaa", "a"}, []string{"a", "a", "a", "a", "a", "aa"}, []string{"aa", "a", "a", "aa", "aa"}, []string{"a", "aa", "a", "aa", "aa"}, []string{"a", "a", "aa", "aa", "aa"}, []string{"aa", "aa", "a", "aa"}, []string{"aaaa", "a", "aa"}, []string{"a", "a", "a", "aa", "aa"}, []string{"aa", "a", "aa", "aa"}, []string{"a", "aa", "aa", "aa"}, []string{"a", "aaaa", "aa"}, []string{"a", "a", "a", "aaaa"}, []string{"aa", "a", "aaaa"}, []string{"a", "aa", "aaaa"}}
+	assert.Equal(t, input, somethingFunkyGoesOn(another_input))
 }
