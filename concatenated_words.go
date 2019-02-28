58bb3a01c430638f238bf1cdf5cb545d083e0a72
mik <mklimoff222@gmail.com>
2019-02-28 14:55:20 +0200

    added problems to work on next

---
+++ concatenated_words.go
@@ -0,0 +1,9 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @ Copyright (c) Michael Leahcim                                                      @
+// @ You can find additional information regarding licensing of this work in LICENSE.md @
+// @ You must not remove this notice, or any other, from this software.                 @
+// @ All rights reserved.                                                               @
+// @@@@@@ At 2019-02-28 14:53 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+package main
+
+// Problem: https://leetcode.com/problems/concatenated-words/
---
+++ count_the_repetitions.go
@@ -0,0 +1,9 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @ Copyright (c) Michael Leahcim                                                      @
+// @ You can find additional information regarding licensing of this work in LICENSE.md @
+// @ You must not remove this notice, or any other, from this software.                 @
+// @ All rights reserved.                                                               @
+// @@@@@@ At 2019-02-28 14:54 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+package main
+
+// Working on https://leetcode.com/problems/count-the-repetitions/
