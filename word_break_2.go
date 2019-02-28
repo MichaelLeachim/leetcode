79ad0b3fef9d7e6ec0e77639de428394e249b8fc
mik <mklimoff222@gmail.com>
2019-02-28 15:36:00 +0200

    continue thinking on word_break_2 problem

--- word_break_2.go
+++ word_break_2.go
@@ -16,21 +16,20 @@ package main
 // Let's define a function `solution` that, given the input,
 // returns the amount of existing solutions.
 
-// solution(catsanddog) = 2
-// solution(catsanddo) = 2
-// solution(catsandd ) = 2
-// solution(catsand  ) = 2 (cat sand, cats and)
-// solution(catsan   ) = 0
-// solution(catsa    ) = 0
-// solution(cats     ) = 2
-// solution(cat      ) = 1
-// solution(ca       ) = 0
-// solution(c        ) = 0
-// solution(         ) = 0
+// solution(catsanddog) = [cats and dog, cat sand dog]
+// solution(catsanddo) =  []
+// solution(catsandd ) =  []
+// solution(catsand  ) =  [cat sand, cats and]
+// solution(catsan   ) =  []
+// solution(catsa    ) =  []
+// solution(cats     ) =  [cats]
+// solution(cat      ) =  [cat]
+// solution(ca       ) =  []
+// solution(c        ) =  []
+// solution(         ) =  []
 
-// So, let's recursively define the solution function
-// solution(x) = if match(suffix_0) != 0 then  match(suffix_0) + solution(len(x)-len(suffix_0)),
-//                match(suffix_1) + solution(len(x)-len(suffix_1))
-//                match(suffix_N) + solution(len(x)-len(suffix_N)))
-
-// where match(suffix) = len(wordDict[suffix])
+// Let's define match function: match(suffix) => wordDict[suffix] or Nothing
+// Let's recursively define solution function:
+// solution(x) =   [each + " " + suffix for each in solution([len(x):-len(suffix)])]  when len(x)-len(suffix) > 0 and match(suffix)
+//                 [suffix]                                                                len(x)-len(suffix) = 0 and match(suffix)
+//                 []
