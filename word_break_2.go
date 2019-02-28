fbb8d47ad0c15a0454c9dded44084a7abbdf76bb
mik <mklimoff222@gmail.com>
2019-02-28 15:18:48 +0200

    thinking on word_break_2 problem

--- word_break_2.go
+++ word_break_2.go
@@ -9,3 +9,28 @@ package main
 
 // Working on this problem:
 // https://leetcode.com/problems/word-break-ii/
+
+// s = "catsanddog"
+// wordDict = ["cat", "cats", "and", "sand", "dog"]
+
+// Let's define a function `solution` that, given the input,
+// returns the amount of existing solutions.
+
+// solution(catsanddog) = 2
+// solution(catsanddo) = 2
+// solution(catsandd ) = 2
+// solution(catsand  ) = 2 (cat sand, cats and)
+// solution(catsan   ) = 0
+// solution(catsa    ) = 0
+// solution(cats     ) = 2
+// solution(cat      ) = 1
+// solution(ca       ) = 0
+// solution(c        ) = 0
+// solution(         ) = 0
+
+// So, let's recursively define the solution function
+// solution(x) = if match(suffix_0) != 0 then  match(suffix_0) + solution(len(x)-len(suffix_0)),
+//                match(suffix_1) + solution(len(x)-len(suffix_1))
+//                match(suffix_N) + solution(len(x)-len(suffix_N)))
+
+// where match(suffix) = len(wordDict[suffix])
