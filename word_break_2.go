9b5fcd83e76c8da97834ab22b20638beaf19212b
mik <mklimoff222@gmail.com>
2019-02-28 14:51:37 +0200

    init

---
+++ word_break_2.go
@@ -0,0 +1,11 @@
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// @ Copyright (c) Michael Leahcim                                                      @
+// @ You can find additional information regarding licensing of this work in LICENSE.md @
+// @ You must not remove this notice, or any other, from this software.                 @
+// @ All rights reserved.                                                               @
+// @@@@@@ At 2019-02-28 14:51 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
+
+package main
+
+// Working on this problem:
+// https://leetcode.com/problems/word-break-ii/
