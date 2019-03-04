1c1839414bf450fb74fb63f0912e1a93bf8c467a
mik <mklimoff222@gmail.com>
2019-03-04 20:30:59 +0200

    added iterative solution for traversal problem

--- binary_tree_postorder_traversal.go
+++ binary_tree_postorder_traversal.go
@@ -13,23 +13,53 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+const isIterative = false
+
 // postorder traversal. first, traverse left subtree, then, traverse right subtree,then, process the root
 func postorderTraversal(root *TreeNode) []int {
-	var traverse func(root *TreeNode)
-	result := []int{}
-	traverse = func(root *TreeNode) {
-		if root == nil {
-			return
+	if isIterative {
+		var root *TreeNode
+		result := []int{}
+		stack := []*TreeNode{root}
+		for len(stack) != 0 {
+			stack, root = stack[:len(stack)-1], stack[len(stack)-1]
+			if root == nil {
+				continue
+			}
+			if root.Left != nil {
+				stack = append(stack, root.Left)
+				continue
+			}
+			if root.Right != nil {
+				stack = append(stack, root.Right)
+				continue
+			}
+			result = append(result, root.Val)
 		}
-		if root.Left != nil {
-			traverse(root.Left)
-		}
-		if root.Right != nil {
-			traverse(root.Right)
-		}
-		result = append(result, root.Val)
+		return result
 	}
-	traverse(root)
 
-	return result
+	// fully working recursive solution
+
+	if !isIterative {
+		result := []int{}
+
+		var traverse func(root *TreeNode)
+		traverse = func(root *TreeNode) {
+			if root == nil {
+				return
+			}
+			if root.Left != nil {
+				traverse(root.Left)
+			}
+			if root.Right != nil {
+				traverse(root.Right)
+			}
+			result = append(result, root.Val)
+		}
+		traverse(root)
+		return result
+	}
+	return []int{}
+
 }
