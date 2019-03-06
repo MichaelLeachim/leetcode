8f40577f2fa513f22f29bb17a52193aabfcde954
mik <mklimoff222@gmail.com>
2019-03-06 18:57:42 +0200

    too difficult, trying another approach

--- largest_rectangle_in_histogram.go
+++ largest_rectangle_in_histogram.go
@@ -7,6 +7,10 @@
 
 package main
 
+import (
+	"log"
+)
+
 // Solving: https://leetcode.com/problems/largest-rectangle-in-histogram/ problem
 
 // solution(X,h) =>
@@ -22,78 +26,108 @@ package main
 //   For given width, largest rectangle is that, whose height is maximum
 ///  For given heigh, largest rectangle is that, which contains maximum width
 
-// best for every index
+// best rectangle for every index in an increasing sequence
 // 5 8 9 8 5
 // 1 2 3 4 5
 
+// solution by stack, and incrementing:
+// Let define a pair, where h is height, and a is area {h,a}
+// The stack will contain pairs [{h,a}...N]
+
+// for every pair in stack: if its height < cur_height, {h,a+h}
+//
+//   if its height >= cur_height, pop stack {cur_height,cur_height*2} multiply current height by two and remove item
+//   push item on the stack
+// if item is smaller or equal the previous:
+//   pop the stack, if larger  than current item:
+//     add height of current to current
+// not working
+
+// given [2, 1, 5, 6, 2, 3]
+// solution(1) = 2, or 2
+// solution(2) = 5, or 3, or 2
+// solution(3) = 6, or 10, or 4, or 2
+
+// 1 1 1 1 1 1
+// solution(0) = 1
+// solution(1) = 2
+// solution(2) = 3
+// solution(3) = 4
+
+// 1 2 3 4 5
+
+// solution(1) = {1,1}
+// solution(2) = {1,2} or {2,2}
+// solution(3) = {1,3} or {2,4} or {3,3}
+// solution(4) = {1,4} or {2,6} or {3,6} or {4,4}
+// solution(5) = {1,5} or {2,8} or {3,9} or {4,8} or {5,5}
+
+// 5 4 3 2 1
+
+// solution(5) = {5,5}
+// solution(4) = {5,5} or {4,8}
+// solution(3) = {5,5} or {4,8} or {3,9}
+// solution(2) = {5,5} or {4,8} or {3,9} or {2,8}
+// solution(1) = {5,5} or {4,8} or {3,9} or {2,8} or {1,5}
+
+// 1 2 3 2 1
+
+// solution(1) = {1,1}
+// solution(2) = {1,2} or {2,2}
+// solution(3) = {1,3} or {2,4} or {3,3}
+// solution(2) = {1,4} or {2,6}
+// solution(1) = {1,5}
+
 func largestRectangleArea(heights []int) int {
-	var largestRectangleAreaSolution func(heights []int, x int, h int) int
-	combineDigits := func(x, y uint32) uint64 {
-		return uint64(x)<<32 | uint64(y)
-	}
-	memoized := map[uint64]int{}
-	largestRectangleAreaSolution = func(heights []int, x int, h int) int {
-		if x < 0 || h > heights[x] {
-			return 0
-		}
-
-		if x == 0 {
-			return h
-		}
-
-		cmb := combineDigits(uint32(x), uint32(h))
-		s, ok := memoized[cmb]
-		if ok {
-			return s
-		}
-		memoized[cmb] = largestRectangleAreaSolution(heights, x-1, h) + h
-		return memoized[cmb]
-	}
-
-	len_height := len(heights)
-	if (len_height) == 0 {
-		return 0
-	}
 	best := 0
-	seen_heights := map[int]bool{}
-	for index, height := range heights {
-		// add simle heuristics ti minimize search?
-		seen_heights[height] = true
+	stack := [][2]int{}
+	new_stack := [][2]int{}
+	var prev_item [2]int
+	for _, cur_height := range heights {
+		cur_area := cur_height
 
-		// no solutions before, so do not count
-		if index-1 < 0 {
-			best = height
-			continue
-		}
-		// if prev height is smaller than current, no point in calculating
-		// upper bounds of current height
-		prev_height := heights[index-1]
-		if height > prev_height && height > best {
-			best = height
-		}
+		for len(stack) > 0 {
+			prev_item, stack = stack[len(stack)-1], stack[:len(stack)-1]
 
-		// no point in calculating heights that haven't been seen before (???)
-		for i := range seen_heights {
-			if i > height {
+			stack_height, stack_area := prev_item[0], prev_item[1]
+
+			if stack_height < cur_height {
+				new_stack = append([][2]int{[2]int{stack_height, stack_area + stack_height}}, new_stack...)
+				if stack_area+stack_height > best {
+					best = stack_area + stack_height
+				}
 				continue
 			}
-			s := largestRectangleAreaSolution(heights, index, i)
-			if s > best {
-				best = s
-				break
+			new_stack = append(new_stack, [2]int{stack_height, new_stack_area})
+
+		}
+		for i := 0; i < len(stack); i++ {
+			// for i := len(stack) - 1; i >= 0; i-- {
+
+			log.Println(stack_height)
+			if stack_height < cur_height {
+				new_stack_area := stack_area + stack_height
+				if new_stack_area > best {
+					best = new_stack_area
+				}
+				// new_stack =
+
+				continue
 			}
+
+			if stack_height >= cur_height {
+				cur_area += cur_height
+			}
+		}
+		new_stack = append(new_stack, [2]int{cur_height, cur_area})
+		stack = new_stack
+		log.Println(stack)
+		if cur_area > best {
+			best = cur_area
 		}
 
 	}
-	return best
-}
 
-func largestRectangleAreaSolution(heights []int, x int, h int) int {
-	if x < 0 || h > heights[x] {
-		return 0
-	}
-	if x == 0 {
-		return h
-	}
-	return largestRectangleAreaSolution(heights, x-1, h) + h
+	return best
+
 }
--- largest_rectangle_in_histogram_test.go
+++ largest_rectangle_in_histogram_test.go
@@ -34,8 +34,18 @@ func TestBinaryCombination(t *testing.T) {
 	tfn((1<<32)-1, (1<<32)-1)
 }
 
+func TestLargestRectangleAreaSpecificCase(t *testing.T) {
+	assert.Equal(t, largestRectangleArea([]int{2, 1, 5, 6, 2, 3}), 10)
+}
+
 func TestLargestRectangleArea(t *testing.T) {
 	assert.Equal(t, largestRectangleArea([]int{2, 1, 5, 6, 2, 3}), 10)
+	assert.Equal(t, largestRectangleArea([]int{2, 1, 2}), 3)
+	assert.Equal(t, largestRectangleArea([]int{3}), 3)
+	assert.Equal(t, largestRectangleArea([]int{3, 1}), 3)
+	assert.Equal(t, largestRectangleArea([]int{3, 1, 1, 1, 1}), 5)
+	assert.Equal(t, largestRectangleArea([]int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}), 10)
+
 	assert.Equal(t, largestRectangleArea([]int{6, 5}), 10)
 
 	assert.Equal(t, largestRectangleArea([]int{6, 1}), 6)
@@ -44,18 +54,12 @@ func TestLargestRectangleArea(t *testing.T) {
 	assert.Equal(t, largestRectangleArea([]int{1, 3}), 3)
 
 	assert.Equal(t, largestRectangleArea([]int{2, 3}), 4)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 0, 1), 1)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 0, 2), 2)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 0, 3), 0)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 1, 3), 3)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 1, 2), 4)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 1, 1), 2)
-	assert.Equal(t, largestRectangleAreaSolution([]int{2, 3}, 1, 0), 0)
 
 	assert.Equal(t, largestRectangleArea([]int{3, 2}), 4)
 
 	assert.Equal(t, largestRectangleArea([]int{5, 6}), 10)
 	assert.Equal(t, largestRectangleArea([]int{1, 2, 3, 4, 5, 6, 1}), 12)
+	assert.Equal(t, largestRectangleArea([]int{1, 2, 3, 4, 5}), 9)
 
 	assert.Equal(t, largestRectangleArea([]int{3, 1}), 3)
 	assert.Equal(t, largestRectangleArea([]int{2}), 2)
@@ -68,12 +72,12 @@ func TestLargestRectangleArea(t *testing.T) {
 	assert.Equal(t, largestRectangleArea([]int{0, 0, 0, 0, 0, 0, 0, 0, 2147483647}), 2147483647)
 
 	assert.Equal(t, largestRectangleArea([]int{0, 0, 0, 0, 0, 0, 10, 110, 2147483647}), 2147483647)
-	assert.Equal(t, largestRectangleArea([]int{2147483647, 0, 2147483647, 0, 2147483647, 0, 2147483647, 0, 2147483647, 0}), 2147483647)
-	larger_input := []int{}
-	for i := 1; i <= 20000; i++ {
-		larger_input = append(larger_input, i)
-	}
-	assert.Equal(t, largestRectangleArea([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}), 110)
-	assert.Equal(t, largestRectangleArea(larger_input), 110)
+	// assert.Equal(t, largestRectangleArea([]int{2147483647, 0, 2147483647, 0, 2147483647, 0, 2147483647, 0, 2147483647, 0}), 2147483647)
+	// larger_input := []int{}
+	// for i := 1; i <= 20000; i++ {
+	// 	larger_input = append(larger_input, i)
+	// }
+	// assert.Equal(t, largestRectangleArea([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}), 110)
+	// assert.Equal(t, largestRectangleArea(larger_input), 110)
 
 }
