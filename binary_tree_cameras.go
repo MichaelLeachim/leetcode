fe92eba10d080b38c8837e6f7934f70ea1ef30ba
mik <mklimoff222@gmail.com>
2019-03-14 12:31:28 +0200

    with this hack doesn't work either

--- binary_tree_cameras.go
+++ binary_tree_cameras.go
@@ -23,12 +23,10 @@ package main
 
 type binary_tree_cameras_ struct {
 	_memoized map[*TreeNode]bool
-	_parentOf map[*TreeNode]*TreeNode
 }
 
 var binary_tree_cameras = binary_tree_cameras_{
 	_memoized: map[*TreeNode]bool{},
-	_parentOf: map[*TreeNode]*TreeNode{},
 }
 
 func (b *binary_tree_cameras_) Solve(t *TreeNode) bool {
@@ -61,68 +59,11 @@ func (b *binary_tree_cameras_) Solve(t *TreeNode) bool {
 
 }
 
-func (b *binary_tree_cameras_) BuildBinaryTreeFromSlice(k int, input []int) *TreeNode {
-	l, r := 2*k, 2*k+1
-	if k < len(input) {
-		if input[k] == -1 {
-			return nil
-		}
-		return &TreeNode{
-			Val:   input[k],
-			Left:  b.BuildBinaryTreeFromSlice(l, input),
-			Right: b.BuildBinaryTreeFromSlice(r, input),
-		}
-	}
-	return nil
-}
-func (b *binary_tree_cameras_) ShowTree(tree *TreeNode, logger func(args ...interface{})) {
-
-	var showrecur func(tree *TreeNode, pad string, name string, logger func(args ...interface{}))
-	showrecur = func(tree *TreeNode, pad string, name string, logger func(args ...interface{})) {
-		if tree != nil {
-			logger(pad, name, tree.Val)
-			showrecur(tree.Left, pad+"-", "[l]", logger)
-			showrecur(tree.Right, pad+"-", "[r]", logger)
-		}
-	}
-	showrecur(tree, "-", "", logger)
-}
-
 func (b *binary_tree_cameras_) CountNodes(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	return 1 + b.CountNodes(root.Left) + b.CountNodes(root.Right)
-}
-
-func (b *binary_tree_cameras_) PatchSolution(parent, root *TreeNode) {
-	if root == nil {
-		return
-	}
-
-	res := !b._memoized[parent] && !b._memoized[root.Left] && !b._memoized[root.Right]
-	b._memoized[root] = res
-	b.PatchSolution(root, root.Left)
-	b.PatchSolution(root, root.Right)
-}
-
-func (b *binary_tree_cameras_) BuildSliceFromBinaryTreeRecursive(data []int, k int, root *TreeNode) {
-	if root == nil {
-		data[k] = -1
-		return
-	}
-	if k < len(data) {
-		data[k] = root.Val
-		b.BuildSliceFromBinaryTreeRecursive(data, k*2+1, root.Left)
-		b.BuildSliceFromBinaryTreeRecursive(data, k*2+2, root.Right)
-	}
-
-}
-
-func (b *binary_tree_cameras_) BuildSliceFromBinaryTree(root *TreeNode) []int {
-	result := make([]int, b.CountNodes(root))
-	b.BuildSliceFromBinaryTreeRecursive(result, 0, root)
-	return result
 }
 
 func minCameraCover(root *TreeNode) int {
@@ -132,7 +73,6 @@ func minCameraCover(root *TreeNode) int {
 
 	// patch those nodes where solution didn't get to
 	// (have no idea, whether it will work, though)
-	binary_tree_cameras.PatchSolution(nil, root)
 	count := 0
 	for _, a := range binary_tree_cameras._memoized {
 		if a == true {
