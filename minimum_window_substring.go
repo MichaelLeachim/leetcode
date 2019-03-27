9c39a83cf3615e934da7031d280cc4c376de2cf6
mik <mklimoff222@gmail.com>
2019-03-27 17:53:25 +0200

    simplifying solution again, this problem looks hard

--- minimum_window_substring.go
+++ minimum_window_substring.go
@@ -6,7 +6,9 @@
 // @@@@@@ At 2019-03-26 15:41 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 package main
 
-import ()
+import (
+	"log"
+)
 
 // Solving: https://leetcode.com/problems/minimum-window-substring/
 // Have two pointers. Have one go through the S, if it sees EOL, break, if it sees symbol in T increment its counter, check
@@ -23,13 +25,65 @@ import ()
 // ADOBECBA   (left pointer)
 // ADOBE CBA  (has solution), best solution CBA
 
-// aaa aa
-//
+// zaaba, aa
+// When exactly solution? when every letter in pattern appears exactly once.
+// When no solution? when there is a letter that appears 0 times, but, a letter can be duplicated, so:
+//   When no solution? when a letter appears less times than it appears in pattern
 
-// When solution? when every letter in pattern appears at least once.
+// Okay, now, it turns out, the pattern can have non unique letters.
+// (Btw, They could have provided an example IN BOLD mentioning it)
+
+// How quickly check if we have a solution?
+// If the pattern is unique, the solution would be the appearance of each letter in pattern at least once.
+// babzaa aab
+// (b|2 a|1) (a|2 b|1)
+// We cannot see the difference by sum: because sum_1==sum_2
+// But, we can mainain a set of 1|0 matches, and its length equals length of unique items in input
+// When do we flip the switch? when item length equal amount of this item on input
+
+// But, if pattern is not unique?
+// aab, aabb a  a2|b1 = 2
+// Well, we can do similar thing:
+//   calculate unique letters in pattern [a,b]
+//   calculate amount each letter appears in pattern [a|2,b|1]
+//   decrement counter by one, when the amount is lower, than the amount of this letter in a pattern
+//   increment counter, by one, when amount is higher than the amount of this letter in a pattern
+
+// aab, aabb
+// solution(aab) = solution(aab)[a|2 b|1] + b
+
+// Naive solution:
+//  for every letter in curinput
+//    for every match, increment match by one.
+//    if match equals amount in pattern, increment sum
+//    if match more than amount in pattern, do nothing
+//
+//    if it is in the pattern, decrement in pattern by one
+//
+// Recursive solution: Solution(baa) = Solution(ba) + a = {b:1,a:1} + a = {b:1,a:2}
+
+func minWindowPatternRecognition(s string, t string) bool {
+	dat := map[rune]int{}
+	for _, v := range t {
+		dat[v] += 1
+	}
+	reso := 0
+	for _, v := range s {
+		_, ok := dat[v]
+		if ok {
+			dat[v] -= 1
+			if dat[v] == 0 {
+				reso += 1
+			}
+
+		}
+	}
+	log.Println(dat, reso)
+	return reso == len(dat)
+}
 
 func minWindow(s string, t string) string {
-	LEN_OF_PATTERN, LEN_OF_INPUT := len(t), len(s)
+	_, LEN_OF_INPUT := len(t), len(s)
 	itemsappeared := 0
 	metitem := map[rune]int{}
 	mustcount := map[rune]int{}
@@ -38,27 +92,30 @@ func minWindow(s string, t string) string {
 		metitem[rune] = 0
 		mustcount[rune] += 1
 	}
+	LEN_OF_PATTERN_UNIQUE := len(mustcount)
+
 	leftp, rightp, bestpatternlen := 0, 0, LEN_OF_INPUT+1
 	bestpattern, curpattern := []rune{}, []rune{}
 
-	for {
-		if leftp >= LEN_OF_INPUT {
-			break
-		}
+	for leftp < LEN_OF_INPUT {
 		rsl := rune(s[leftp])
 		// if has solution, advance left
-		if itemsappeared == LEN_OF_PATTERN {
+		if itemsappeared == LEN_OF_PATTERN_UNIQUE {
+			log.Println(string(curpattern), t, itemsappeared)
 			lcurpat := len(curpattern)
 
-			if lcurpat == LEN_OF_PATTERN {
-				return string(curpattern)
-			}
+			// if lcurpat == LEN_OF_PATTERN {
+			// 	return string(curpattern)
+			// }
 			// bookkeep best pattern
 			if bestpatternlen > lcurpat {
 				bestpattern = curpattern
 				bestpatternlen = lcurpat
 			}
-			curpattern = curpattern[1:]
+			if len(curpattern) != 0 {
+				curpattern = curpattern[1:]
+			}
+
 			leftp += 1
 			c, ok := metitem[rsl]
 			if ok {
@@ -81,7 +138,7 @@ func minWindow(s string, t string) string {
 		// bookkeep solution
 		c, ok := metitem[rsr]
 		if ok {
-			if c == 0 {
+			if c == mustcount[rsl] {
 				itemsappeared += 1
 			}
 			metitem[rsr] += 1
--- minimum_window_substring_test.go
+++ minimum_window_substring_test.go
@@ -12,12 +12,21 @@ import (
 	"testing"
 )
 
-func TestMinWindowFailingCase(t *testing.T) {
-	assert.Equal(t, "A", minWindow("A", "A"))
+func TestMinWindowPatternRecognition(t *testing.T) {
+	assert.Equal(t, true, minWindowPatternRecognition("babzaa", "aab"))
+
+	assert.Equal(t, true, minWindowPatternRecognition("bbbaaaa", "ab"))
+	assert.Equal(t, false, minWindowPatternRecognition("ab", "aaabbbb"))
+
+	assert.Equal(t, true, minWindowPatternRecognition("ba", "ab"))
+	assert.Equal(t, true, minWindowPatternRecognition("baa", "aab"))
+	assert.Equal(t, true, minWindowPatternRecognition("baaaaaaaaaaaaaaaaaaaa", "aab"))
+	assert.Equal(t, false, minWindowPatternRecognition("baaaaaaaaaaaaaaaaaaaaz", "aabzz"))
 
 }
 
 func TestMinWindow(t *testing.T) {
+	assert.Equal(t, "A", minWindow("A", "A"))
 	assert.Equal(t, "BANC", minWindow("ADOBECODEBANC", "ABC"))
 	assert.Equal(t, "CBA", minWindow("ADOBECBA", "ABC"))
 
@@ -26,14 +35,31 @@ func TestMinWindow(t *testing.T) {
 	assert.Equal(t, "A", minWindow("ABBABBAABBBBB", "A"))
 
 	assert.Equal(t, "AA", minWindow("AA", "AA"))
+
 	assert.Equal(t, "aaa", minWindow("aaa", "aaa"))
+
 	assert.Equal(t, "a", minWindow("aaa", "a"))
 	assert.Equal(t, "aa", minWindow("aaa", "aa"))
+	assert.Equal(t, "aa", minWindow("zaaba", "aa"))
+	assert.Equal(t, "aa", minWindow("zaa", "aa"))
+
+	assert.Equal(t, "aabb", minWindow("aabbb", "aabb"))
+
+	assert.Equal(t, "aabb", minWindow("zaabbb", "aabb"))
 
 	assert.Equal(t, "aba", minWindow("abbbaaababa", "aba"))
+
+	assert.Equal(t, "", minWindow("abb", "aba"))
 
 	assert.Equal(t, "A", minWindow("A", "A"))
 	assert.Equal(t, "", minWindow("", "A"))
 	assert.Equal(t, "", minWindow("", ""))
+	assert.Equal(t, "aa", minWindow("zaa", "aa"))
+	assert.Equal(t, "aa", minWindow("aaz", "aa"))
+	assert.Equal(t, "aa", minWindow("zaaz", "aa"))
+	assert.Equal(t, "aa", minWindow("zaazaa", "aa"))
+	assert.Equal(t, "aa", minWindow("zaazaa", "aa"))
+	assert.Equal(t, "aa", minWindow("aazaa", "aa"))
+	assert.Equal(t, "abza", minWindow("babzaa", "aab"))
 
 }
