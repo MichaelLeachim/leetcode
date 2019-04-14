db40aacff7d03944051a3fc781e95beca65f345b
mik <mklimoff222@gmail.com>
2019-04-14 08:26:50 +0300

    almost solved

--- python/devtest/devtest.py
+++ python/devtest/devtest.py
@@ -5,6 +5,13 @@
 # @ All rights reserved.                                                               @
 # @@@@@@ At 2019-04-14 08:19 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 from collections import OrderedDict
+import ipdb
+
+# Okay, how to solve it?
+
+# For every item, if previous hours match current hours,
+# Then, merge previous item with current item
+# Otherwise add them to array.
 
 class Restaurant():
   WEEKDAYS = [
@@ -23,6 +30,24 @@ class Restaurant():
         self.WEEKDAYS,
         [opening_hour.to_string() for opening_hour in opening_hours])
   def get_opening_hours(self):
+    # ipdb.set_trace()
+    s = self.opening_hours
+    solution = s[0]
+    
+    for index, [day,hours] in enumerate(s[1:]):
+      index = index+1
+      [prevday, prevhours] = solution[index-1]
+      if prevhours == hours:
+        prevday = prevday + " - " + day
+        solution[index-1] = [prevday,hours]
+        continue
+      solution.append(day,hours)
+          
+    
+    
+
+    
+    
     return ""
   
 class OpeningHour():
